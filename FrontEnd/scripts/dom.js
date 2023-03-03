3c1fbffda47565725e6cd3f421f04b9917bd3c22
pierrardp <pierrardp@laposte.net>
2023-03-03 11:57:39 +0100

    feat: filtres des projets

--- FrontEnd/scripts/dom.js
+++ FrontEnd/scripts/dom.js
@@ -15,7 +15,7 @@ function displayGallery(worksSent) {
 
   const categoriesNames = new Set();
   // affichage des travaux : image + titre
-  
+
   // récup des catégories dans l'objet Set
   for (let i in worksSent) {
     let workSenti = worksSent[i];
@@ -38,4 +38,46 @@ function displayGallery(worksSent) {
   }
   // renvoi des catégories pour l'affichage des filtres
   return (categoriesNames);
+}
+
+
+// AFFICHAGE DES FILTRES
+function displayFilters(works, categories) {
+  const portfolio = document.getElementById("portfolio");
+
+  // conteneur filtres rattaché au portfolio
+  const filtersContainer = createElement("div", ["filters"]);
+  const ngallery = document.getElementsByClassName("gallery")[0];
+  portfolio.insertBefore(filtersContainer, ngallery);
+
+  // ajout du bouton "Tous"
+  const btnAll = createElement("button", ["btn", "SelectedFilter"]);
+  btnAll.innerText = "Tous";
+  filtersContainer.appendChild(btnAll);
+  btnAll.addEventListener("click", function () {
+    displayGallery(works);
+    updateFilterBtn(btnAll);
+  })
+
+  // affichage des boutons de filtre
+  for (let categoryName of categories) {
+    const catButton = createElement("button", ["btn"]);
+    catButton.innerText = categoryName.name;
+    filtersContainer.appendChild(catButton);
+
+    catButton.addEventListener("click", function () {
+      const FilteredWorks = works.filter(work => work.category.name == categoryName.name);
+      displayGallery(FilteredWorks);
+      updateFilterBtn(catButton);
+    });
+  }
+}
+
+// MISE A JOUR DE L'APPARENCE DES BOUTONS FILTRES
+function updateFilterBtn(filterBtn) {
+  const filters = document.getElementsByClassName("btn");
+  for (i = 0; i < filters.length; i++) {
+    filters[i].classList.remove("SelectedFilter");
+  }
+  filterBtn.classList.add("SelectedFilter");
 }
--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -4,16 +4,36 @@ async function fetchWorks() {
     if (!response.ok) {
       throw new Error(`HTTP erreur ${response.status} !`);
     }
-    const data = await response.json();
-    return data;
+    const works = await response.json();
+    return works;
   }
   catch (error) {
     console.error(error);
   }
 }
 
-fetchWorks().then(works => {
+fetchWorks().then(worksFetched => {
   // AFFICHAGE DU PORTFOLIO
-  displayGallery(works);
+  displayGallery(worksFetched);
+
+  // AFFICHAGE DES FILTRES
+  async function fetchCategories() {
+    try {
+      const response = await fetch("http://localhost:5678/api/categories");
+      if (!response.ok) {
+        throw new Error(`HTTP erreur ${response.status} !`);
+      }
+      const categories = await response.json();
+      return categories;
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+  fetchCategories().then(categoriesFetched => {
+    // AFFICHAGE DU PORTFOLIO
+    displayFilters(worksFetched,categoriesFetched);
+  })
+
 
 })
